a80363c368138b978e7bc24c4b3261f536ca8386
Morgen Nicodemus <reidnicodemus@gmail.com>
2026-04-24 14:07:57 -0500

    Ternary

--- Tidbit1.c
+++ Tidbit1.c
@@ -5,9 +5,10 @@ int main(void)
     int i = 2;
     float f = 3.14;
     char *s = "Hello, world!";  // char * ("char pointer") is the string type
-    bool x = true;
+    int x = 78;
 
     if (x) {
         printf("%s  i = %d and f = %f!\n", s, i, f);
+        printf("The number %d is %s.\n", x, x % 2 == 0? "even": "odd");
     }
 }
