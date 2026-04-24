813249502b0b6e8c52d2ba32586606b883127b0a
Morgen Nicodemus <reidnicodemus@gmail.com>
2026-04-24 13:59:39 -0500

    Throw a bool in there

--- Tidbit1.c
+++ Tidbit1.c
@@ -5,6 +5,9 @@ int main(void)
     int i = 2;
     float f = 3.14;
     char *s = "Hello, world!";  // char * ("char pointer") is the string type
+    bool x = true;
 
-    printf("%s  i = %d and f = %f!\n", s, i, f);
+    if (x) {
+        printf("%s  i = %d and f = %f!\n", s, i, f);
+    }
 }
